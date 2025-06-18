1151594ad2b1046f59d9b6c6590b3cc5fa6563b7
Jaydeep Shome <jaydeepshome06@gmail.com>
2025-06-18 12:37:59 +0530

    Added script for the password checker and readme file

---
+++ python/password_checker/README.md
@@ -0,0 +1,7 @@
+```The script checks for the paswword strength given
+1. Takes input without showing it on screen.
+2. Analyzes password contents (lowercase, uppercase, digits, spaces, special characters).
+3. Rates password strength on a scale of 1 to 5
+4. Gives feedback based on strength
+5. Repeats if the user wants to check another password
+```
---
+++ python/password_checker/main.py
@@ -0,0 +1,55 @@
+import string
+import getpass
+
+def check_password():
+    password = getpass.getpass("Enter your password: ")
+    strength = 0
+    remarks = ''
+
+    lower_count = sum(1 for c in password if c.islower())
+    upper_count = sum(1 for c in password if c.isupper())
+    num_count = sum(1 for c in password if c.isdigit())
+    wspace_count = sum(1 for c in password if c.isspace())
+    special_count = sum(1 for c in password if c in string.punctuation)
+
+    strength += bool(lower_count)
+    strength += bool(upper_count)
+    strength += bool(num_count)
+    strength += bool(wspace_count)
+    strength += bool(special_count)
+
+    remarks_dict = {
+        1: "Very Bad Password!!! Change ASAP",
+        2: "Not a good password!!! Change ASAP",
+        3: "It's a weak password, consider changing",
+        4: "It's a hard password, can be better",
+        5: "A very Strong Password"
+    }
+
+    remarks = remarks_dict.get(strength, "Unable to evaluate password")
+
+    print("\nYour password contains: ")
+    print(f" {lower_count} lowercase character")
+    print(f" {upper_count} uppercase character")
+    print(f" {num_count} numeric character")
+    print(f" {wspace_count} whitespace character")
+    print(f" {special_count} special character")
+
+    print(f"\nPassword Strength: {strength}/5")
+    print(f"Hint: {remarks}\n")
+
+def ask_to_continue(prompt):
+    while True:
+        choice = input(prompt + " (y/n): ").strip().lower()
+        if choice == 'y':
+            return True
+        elif choice == 'n':
+            return False
+        else:
+            print("Invalid input. Please enter 'y' or 'n'. ")
+
+
+if __name__ == '__main__':
+    print("+++ Welcome to the password strength checker +++\n")
+    while ask_to_continue("Do you want to check password"):
+        check_password()
